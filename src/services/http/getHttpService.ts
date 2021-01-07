51e891119dd2ff338f9fbe534250d8f8e31ff840
Naylepsh <naylepsh@gmail.com>
2021-01-07 21:39:32 +0100

    add comment regarding new changes

--- src/services/http/getHttpService.ts
+++ src/services/http/getHttpService.ts
@@ -4,6 +4,12 @@ export interface HttpGetOptions {
   headers: any;
 }
 
+/* 
+To properly integrate with React Query,
+this function cannot work the same as other mutating http methods do.
+Wrapping it with HttpResponse would require to throw on error to use ReactQuery isError methods and so on.
+I don't think increasing consitency is worth having to deal with React Query antics in this case.
+*/
 export async function httpGet<ResponseType>(
   url: string,
   config?: HttpGetOptions
