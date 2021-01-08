5824335ea041bc6ee1790037fa0cf5e8587c344a
Naylepsh <naylepsh@gmail.com>
2021-01-08 15:23:52 +0100

    more controller comments

--- src/services/gw2lfg-server/raid-bosses/getRaidBossesService.ts
+++ src/services/gw2lfg-server/raid-bosses/getRaidBossesService.ts
@@ -2,19 +2,9 @@ import { httpGet } from "../../http/getHttpService";
 import { gw2lfgUrl } from "../constants";
 import { RaidBossDTO } from "../entities/RaidBossDTO";
 
-interface GetRaidBossesDTO {
-  take?: number;
-}
-
 // sends GET /raid-bosses request to gw2lfg-server
-export async function getRaidBosses(dto?: GetRaidBossesDTO) {
-  // assign default values if dto is lacking following properties
-  const defaultNumberToTake = 42;
-  const take = dto?.take ?? defaultNumberToTake;
-
-  const url = `${raidBossesUrl}?take=${take}`;
-
-  const { data } = await httpGet<{ data: RaidBossDTO[] }>(url);
+export async function getRaidBosses() {
+  const { data } = await httpGet<{ data: RaidBossDTO[] }>(raidBossesUrl);
 
   return data;
 }
