2ddf7820467bd3cfa0cb5f3e56241baa9484127a
Naylepsh <naylepsh@gmail.com>
2021-01-14 12:49:08 +0100

    better styles for user posts

--- src/components/User/UserRaidPosts.tsx
+++ src/components/User/UserRaidPosts.tsx
@@ -1,10 +1,11 @@
-import { Box, Button } from "@material-ui/core";
+import { Box, Button, Typography, Container, Paper } from "@material-ui/core";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
 import Loading from "../common/Loading/Loading";
 import { RaidPostDTO } from "../../services/gw2lfg-server/entities/RaidPostDTO";
 import { RaidPost } from "../RaidPost/RaidPost";
 import { useGetUserRaidPostsQuery } from "../../hooks/queries/users/useGetUserRaidPostsQuery";
+import { useGetUserProfileQuery } from "../../hooks/queries/users/useGetUserProfileQuery";
 
 /* 
 Paginated User Raid Posts component.
@@ -16,17 +17,24 @@ export default function UserRaidPosts() {
   const [page, setPage] = useState(1);
   const [prevRaidPosts, setPrevRaidPosts] = useState([] as RaidPostDTO[]);
   const {
-    isLoading,
-    isError,
+    isLoading: areUserPostsLoading,
+    isError: hasUserPostFailed,
     error,
-    data,
+    data: userPosts,
     isPreviousData,
   } = useGetUserRaidPostsQuery(id as string, page);
+  const {
+    isLoading: isUserLoading,
+    isError: hasUserProfileFailed,
+    data: profile,
+  } = useGetUserProfileQuery(id as string);
 
+  const isLoading = areUserPostsLoading || isUserLoading;
   if (isLoading) {
     return <Loading size="large" />;
   }
 
+  const isError = hasUserPostFailed || hasUserProfileFailed;
   if (isError) {
     console.log({ error });
     return <div>Error occured ...</div>;
@@ -34,7 +42,7 @@ export default function UserRaidPosts() {
 
   const currentRaidPosts = isPreviousData
     ? prevRaidPosts
-    : [...prevRaidPosts, ...data.raidPosts];
+    : [...prevRaidPosts, ...userPosts.raidPosts];
 
   const loadMore = () => {
     setPage(page + 1);
@@ -42,20 +50,27 @@ export default function UserRaidPosts() {
   };
 
   return (
-    <Box my={5}>
-      {currentRaidPosts.map((raidPost) => (
-        <RaidPost raidPost={raidPost} key={raidPost.id} />
-      ))}
-      {data?.hasMore && (
-        <Button
-          variant="contained"
-          color="secondary"
-          fullWidth
-          onClick={loadMore}
-        >
-          Load more
-        </Button>
-      )}
-    </Box>
+    <Container disableGutters component={Paper}>
+      <Box my={3} pt={3}>
+        <Box mb={3} display="flex" justifyContent="center" alignItems="center">
+          <Typography variant="h6">
+            {profile.user.username}'s raid posts
+          </Typography>
+        </Box>
+        {currentRaidPosts.map((raidPost) => (
+          <RaidPost raidPost={raidPost} key={raidPost.id} />
+        ))}
+        {userPosts?.hasMore && (
+          <Button
+            variant="contained"
+            color="secondary"
+            fullWidth
+            onClick={loadMore}
+          >
+            Load more
+          </Button>
+        )}
+      </Box>
+    </Container>
   );
 }
