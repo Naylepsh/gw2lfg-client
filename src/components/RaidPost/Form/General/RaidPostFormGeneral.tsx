b43197b5c3baaaafcf0b8bc2c1d94f97530618a3
Naylepsh <naylepsh@gmail.com>
2021-05-18 14:36:56 +0200

    use form control for raid general

--- src/components/RaidPost/Form/General/RaidPostFormGeneral.tsx
+++ src/components/RaidPost/Form/General/RaidPostFormGeneral.tsx
@@ -1,4 +1,11 @@
-import { Typography, Box, Grid } from "@material-ui/core";
+import {
+  FormControl,
+  FormLabel,
+  FormGroup,
+  Grid,
+  makeStyles,
+  createStyles,
+} from "@material-ui/core";
 import React from "react";
 import MuiDateTimePicker from "../../../common/inputs/MuiDateTimePicker";
 import SelectInput from "../../../common/inputs/SelectInput";
@@ -22,35 +29,48 @@ export default function RaidPostFormGeneral(props: RaidPostFormGeneralProps) {
     { label: "NA", value: "NA" },
   ];
 
+  const classes = useStyles();
+
   return (
-    <Box
-      my={3}
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Typography variant="h6">General</Typography>
-      <Grid container justify="space-between">
-        <Grid item xs={12} sm={2}>
-          <SelectInput
-            name={serverId}
-            items={servers}
-            label="Server"
-            required
-          />
-          <MuiDateTimePicker
-            id={dateId}
-            label="Date"
-            value={dateSelected}
-            onChange={onChange}
-            required
-          />
+    <FormControl className={classes.formControl}>
+      <FormLabel component="legend">General</FormLabel>
+      <FormGroup className={classes.formGroup}>
+        <Grid container justify="space-between">
+          <Grid item xs={12} sm={2}>
+            <SelectInput
+              name={serverId}
+              items={servers}
+              label="Server"
+              required
+            />
+            <MuiDateTimePicker
+              id={dateId}
+              label="Date"
+              value={dateSelected}
+              onChange={onChange}
+              required
+            />
+          </Grid>
+          <Grid item xs={12} sm={8}>
+            <RaidPostFormDescription name={descriptionId} />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={8}>
-          <RaidPostFormDescription name={descriptionId} />
-        </Grid>
-      </Grid>
-    </Box>
+      </FormGroup>
+    </FormControl>
   );
 }
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    formControl: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      margin: "30px",
+    },
+    formGroup: {
+      width: "100%",
+    },
+  })
+);
