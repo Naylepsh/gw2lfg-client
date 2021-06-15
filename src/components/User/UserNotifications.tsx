1f0f9c34ad4d4f177d2ea74bc08c909e49d0e375
Naylepsh <naylepsh@gmail.com>
2021-06-15 22:23:56 +0200

    linkify chunks of notifications

--- src/components/User/UserNotifications.tsx
+++ src/components/User/UserNotifications.tsx
@@ -1,6 +1,5 @@
-import { ListItem, ListItemText } from "@material-ui/core";
+import { ListItem, ListItemText, makeStyles } from "@material-ui/core";
 import { Box, Container, List, Paper, Divider } from "@material-ui/core";
-import { useRouter } from "next/router";
 import React from "react";
 import { useGetNotificationsQuery } from "../../hooks/queries/notifications/useGetNotificationsQuery";
 import { useIsAuthenticated } from "../../hooks/useIsAuthenticated";
@@ -17,6 +16,8 @@ export default function UserNotifications() {
     data,
   } = useGetNotificationsQuery({ recipent: user.username }, 1);
 
+  const classes = useStyles();
+
   const isLoading = isUserLoading || areNotificationsLoading;
   const isError = isErrorOnUser || isErrorOnNotifications;
 
@@ -26,19 +27,50 @@ export default function UserNotifications() {
   const { notifications } = data;
 
   return (
-    <Container maxWidth="sm" component={Paper}>
-      <Box my={3} py={3}>
+    <Container maxWidth="sm" component={Paper} className={classes.container}>
+      <Box my={3}>
         <List>
-          {notifications.map((notification, i) => (
-            <React.Fragment key={notification.id}>
-              <ListItem>
-                <ListItemText primary={notification.text} />
-              </ListItem>
-              {i < notifications.length - 1 && <Divider />}
-            </React.Fragment>
-          ))}
+          {notifications.map((notification, i) => {
+            const text = parseText(notification.text);
+
+            return (
+              <React.Fragment key={notification.id}>
+                <ListItem className={classes.seenNotification}>
+                  <ListItemText primary={text} />
+                </ListItem>
+                {i < notifications.length - 1 && <Divider />}
+              </React.Fragment>
+            );
+          })}
         </List>
       </Box>
     </Container>
   );
 }
+
+function parseText(text: string) {
+  return <span>{text.split(" ").map(parseChunk)}</span>;
+}
+
+function parseChunk(chunk: string) {
+  if (chunk.toLowerCase().startsWith("user#")) {
+    const id = chunk.slice(5);
+    return <a href={`/users/${id}`}>User#{id}</a>;
+  }
+
+  if (chunk.toLowerCase().startsWith("post#")) {
+    const id = chunk.slice(5);
+    return <a href={`/raid-posts/${id}`}>Raid Post#{id}</a>;
+  }
+
+  return chunk + " ";
+}
+
+const useStyles = makeStyles(() => ({
+  seenNotification: {
+    background: "#f6f8fa",
+  },
+  container: {
+    padding: "0",
+  },
+}));
